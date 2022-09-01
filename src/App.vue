75f056ed2799a0072b46edbb24832f116717f03a
Branlice <lishiwen@wclound.cn>
2022-09-02 00:23:13 +0800

    feat: integrated pinia state management

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import HelloWorldVue from "@components/HelloWorld.vue";
+import { useLocaleStore } from "@store/locale";
+
+const localeStore = useLocaleStore();
 </script>
 
 <template>
   <div>
+    <button @click="localeStore.setLocale(Date.now().toString())">
+      {{ localeStore.locale }}
+    </button>
+
     <HelloWorldVue />
   </div>
 </template>
