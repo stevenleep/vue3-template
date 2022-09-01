ee0e03f93bbf29f421675212bad980219269ee89
Branlice <lishiwen@wclound.cn>
2022-09-01 21:02:24 +0800

    feat: happy path

---
+++ src/utils/appTitle.ts
@@ -0,0 +1,11 @@
+let oldTitle = getAppTitle();
+
+export function setAppTitle(title: string): void {
+  document.title = title;
+}
+export function getAppTitle(): string {
+  return document.title;
+}
+export function resetAppTitle() {
+  setAppTitle(oldTitle);
+}
