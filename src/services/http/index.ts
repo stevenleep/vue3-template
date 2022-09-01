6c9031d25e997117d2eb2ff1e4f2c4d0ae531bd5
Branlice <lishiwen@wclound.cn>
2022-09-02 00:55:32 +0800

    feat: add element-plus and axios

---
+++ src/services/http/index.ts
@@ -0,0 +1,12 @@
+import { getEnv } from "@/utils";
+import axios from "axios";
+
+// XXX: 临时解决方案
+export function createHttpService(currentRuntimeBaseURL:string) {
+    return axios.create({
+        baseURL: getEnv(currentRuntimeBaseURL),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
