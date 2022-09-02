f4b5949b3f7161360511822b5f6ba187533c0ed8
Branlice <lishiwen@wclound.cn>
2022-09-02 13:39:29 +0800

    feat: add hooks directory and case

---
+++ src/hooks/useCopy.ts
@@ -0,0 +1,24 @@
+import { getElementTextContent, isElement } from "@/utils";
+
+export function useWriteTextClipboard(text: string): Promise<void> {
+  /* Copy the text inside the text field */
+  return navigator.clipboard.writeText(text);
+}
+
+export function useReadTextClipboard(): Promise<string> {
+  return navigator.clipboard.readText();
+}
+
+/**
+ *
+ * @param element HTMLElement
+ * @returns Promise<void>
+ */
+export function useCopy<E extends HTMLElement>(element: E): Promise<void>;
+export function useCopy(element: string): Promise<void> {
+  if (isElement(element)) {
+    const text = getElementTextContent(element).toString();
+    return useWriteTextClipboard(text);
+  }
+  return useWriteTextClipboard(element);
+}
---
+++ src/hooks/useSelection.ts
@@ -0,0 +1,29 @@
+import { getElementTextContent, isSelectableElement } from "@/utils";
+import { Ref } from "vue";
+
+// export interface SelectionOptions {
+//   onlyText?: boolean; // default: true
+// }
+export function useSelection<SE extends HTMLElement = HTMLElement>(
+  ref: Ref<SE>,
+  //   options?: SelectionOptions,
+): string {
+  const element = ref.value;
+  if (!element) {
+    throw new Error("Element is not defined");
+  }
+
+  let clipboardText = "";
+
+  if (isSelectableElement(element)) {
+    element.select();
+    element.setSelectionRange(0, element.value.length);
+
+    clipboardText = element.value;
+  } else {
+    clipboardText = getElementTextContent(element).toString();
+    // TODO: 应该使用insertAPI将内容设置到 Selection Text
+  }
+
+  return clipboardText;
+}
