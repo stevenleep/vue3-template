efec6ef57076d0ca31ab8611be26fb7b75fb004d
Branlice <lishiwen@wclound.cn>
2022-09-02 16:28:55 +0800

    feat: integrated @vueuse/core and @vueuse/components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,12 @@
 <template>
   <div>HelloWorld</div>
+  <OnClickOutside @trigger="counter++">
+    <div>Click Outside of Me: {{ counter }}</div>
+  </OnClickOutside>
 </template>
+
+<script setup lang="ts">
+import { OnClickOutside } from "@vueuse/components";
+import { ref } from "vue";
+const counter = ref(0);
+</script>
