a510470f4f4fcd701efaa5680854ea1d7628780c
Branlice <lishiwen@wclound.cn>
2022-09-02 10:20:37 +0800

    fix: repair the vscode/ide automatic verification

---
+++ shims.d.ts
@@ -0,0 +1,13 @@
+/**
+ * why need this?
+ *
+ * @description
+ *
+ * because of the TS-PARSER problem, it will make the UNPLugin-Vue-components automatically
+ * find the type definition of the type of no .vue file
+ */
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
